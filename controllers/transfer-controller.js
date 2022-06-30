8c219ce171c88c2b8b84a3652179697bc66d4642
aframuz <aframuz@tutanota.io>
2022-06-30 19:59:30 -0400

    Added controllers

--- controllers/transfer-controller.js
+++ controllers/transfer-controller.js
@@ -2,23 +2,39 @@
 =              IMPORT MODULES                =
 =============================================*/
 // Local modules
+const db = require("../models/transfer")
 
 /*=============================================
 =                  HANDLERS                   =
 =============================================*/
 // Get
-const get = async (req, res) => {}
+const get = async (req, res) => {
+   try {
+      const transfers = await db.getTransfers()
+      res.status(200).json(transfers)
+   } catch (err) {
+      console.error(`Error getting transfers from DB:\n${err}`)
+      res.status(500).send("Error getting transfers")
+   }
+}
 
 // Inserting
-const add = async (req, res) => {}
+const add = async (req, res) => {
+   const newTransfer = req.body
 
-// Updating
-const update = async (req, res) => {}
-
-// Deleting
-const deleteS = async (req, res) => {}
+   try {
+      const insertedTransfer = await db.insertTransfer(newTransfer)
+      res.status(201).json(insertedTransfer)
+   } catch (err) {
+      console.error("Error adding transfer to DB:\n${err}")
+      res.status(500).send("Error adding transfer")
+   }
+}
 
 /*=============================================
 =                   EXPORTS                   =
 =============================================*/
-module.exports = {}
+module.exports = {
+   get,
+   add,
+}
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,23 +2,70 @@
 =              IMPORT MODULES                =
 =============================================*/
 // Local modules
+const db = require("../models/user")
 
 /*=============================================
 =                  HANDLERS                   =
 =============================================*/
 // Get
-const get = async (req, res) => {}
+const get = async (req, res) => {
+   try {
+      const users = await db.getUser()
+      res.status(200).json(users)
+   } catch (err) {
+      console.error(`Error getting users from DB:\n${err}`)
+      res.status(500).send("Error getting users")
+   }
+}
 
 // Inserting
-const add = async (req, res) => {}
+const add = async (req, res) => {
+   const newUser = req.body
+
+   try {
+      const insertedUser = await db.insertUser(newUser)
+      res.status(201).json(insertedUser)
+   } catch (err) {
+      console.error(`Error adding user to DB:\n${err}`)
+      res.status(500).send("Error adding user")
+   }
+}
 
 // Updating
-const update = async (req, res) => {}
+const update = async (req, res) => {
+   const newUserData = {
+      id: req.params.id,
+      ...req.body,
+   }
+
+   try {
+      const updatedUser = await db.updateUser(newUserData)
+      res.status(201).json(updatedUser)
+   } catch (err) {
+      console.error(`Error updating user in DB:\n${err}`)
+      res.status(500).send("Error updating user")
+   }
+}
 
 // Deleting
-const deleteS = async (req, res) => {}
+const deleteU = async (req, res) => {
+   const id = req.params.id
+
+   try {
+      const deletedUser = await db.deleteUser(id)
+      res.status(201).json(deletedUser)
+   } catch (err) {
+      console.error(`Error deleting user from DB:\n${err}`)
+      res.status(500).send("Error deleting user")
+   }
+}
 
 /*=============================================
 =                   EXPORTS                   =
 =============================================*/
-module.exports = {}
+module.exports = {
+   get,
+   add,
+   update,
+   deleteU,
+}
