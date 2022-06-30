23074ba0d118f63c8206402def3066df3c5c44d2
aframuz <aframuz@tutanota.io>
2022-06-30 19:32:17 -0400

    Added DB models

--- models/transfer.js
+++ models/transfer.js
@@ -7,18 +7,45 @@ const client = require("../config/db.config")
 /*=============================================
 =                   QUERIES                   =
 =============================================*/
-// Get
-const get = async () => {}
+// Get, RETURN (date, sender Name, receiver Name, amount)
+const get = async () => {
+   // Query Conf
+   const queryConf = {
+      text: "SELECT tr.fecha, u1.nombre as emisor, u2.nombre as receptor, tr.monto FROM transferencias tr LEFT JOIN usuarios u1 ON u1.id = tr.emisor LEFT JOIN usuarios u2 ON u2.id = tr.receptor;",
+   }
 
-// Insert
-const insert = async (obj) => {}
+   try {
+      const transfers = await client.query(queryConf)
+      return transfers.rows
+   } catch (err) {
+      console.error(`Error getting transfers from DB:\n${err}`)
+      throw err
+   }
+}
 
-// Update
-const update = async (obj) => {}
+// Insert transfer
+const insert = async (obj) => {
+   // Queries Conf
+   const insertTransfer = {
+      text: "INSERT INTO transferencias (emisor, receptor, monto, fecha) VALUES ($1, $2, $3, NOW()) RETURNING *",
+      values: Object.values(obj),
+   }
+   const addBalanceSend = {
+      text: "UPDATE usuarios SET balance = balance - $3 WHERE id = $1",
+   }
+   const subBalanceReceiv = {
+      text: "UPDATE usuarios SET balance = balance + $3 WHERE id = $2",
+   }
 
-// Delete
-const deleteS = async (id) => {}
-
+   try {
+      const res = await client.transaction([insertTransfer, addBalanceSend, subBalanceReceiv])
+      console.log(`transfer added to DB`)
+      return res.rows[0]
+   } catch (err) {
+      console.error(`Error adding transfer to DB:\n${err}`)
+      throw err
+   }
+}
 /*=============================================
 =                   EXPORTS                   =
 =============================================*/
--- models/user.js
+++ models/user.js
@@ -8,18 +8,81 @@ const client = require("../config/db.config")
 =                   QUERIES                   =
 =============================================*/
 // Get
-const get = async () => {}
+const get = async () => {
+   // Query Conf
+   const queryConf = {
+      text: "SELECT * FROM usuarios",
+   }
+
+   try {
+      const users = await client.query(queryConf)
+      return users.rows
+   } catch (err) {
+      console.error(`Error getting users from DB:\n${err}`)
+      throw err
+   }
+}
 
 // Insert
-const insert = async (obj) => {}
+const insert = async (obj) => {
+   // Query Conf
+   const queryConf = {
+      text: "INSERT INTO usuarios (nombre, balance) VALUES ($1, $2) RETURNING *",
+      values: Object.values(obj),
+   }
+
+   try {
+      const res = await client.query(queryConf)
+      console.log(`User added to DB`)
+      return res.rows[0]
+   } catch (err) {
+      console.error(`Error adding user to DB:\n${err}`)
+      throw err
+   }
+}
 
 // Update
-const update = async (obj) => {}
+const update = async (obj) => {
+   // Query conf
+   const queryConf = {
+      text: "UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *",
+      values: Object.values(obj),
+   }
+
+   try {
+      const res = await client.query(queryConf)
+      console.log(`User updated in DB`)
+      return res.rows[0]
+   } catch (err) {
+      console.error(`Error updating user in DB:\n${err}`)
+      throw err
+   }
+}
 
 // Delete
-const deleteS = async (id) => {}
+const deleteU = async (id) => {
+   // Query conf
+   const queryConf = {
+      text: "DELETE FROM usuarios WHERE id = $1",
+      values: [id],
+   }
+
+   try {
+      const res = await client.query(queryConf)
+      console.log(`User deleted from DB`)
+      return res.rows[0]
+   } catch (err) {
+      console.error(`Error deleting user from DB:\n${err}`)
+      throw err
+   }
+}
 
 /*=============================================
 =                   EXPORTS                   =
 =============================================*/
-module.exports = {}
+module.exports = {
+   get,
+   insert,
+   update,
+   deleteU,
+}
